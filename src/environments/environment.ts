4bf1b80aa3ad681912634ee4bea44698e0c9baa4
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-08 00:52:27 -0300

    feat: usuarios crud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: "https://backend-alunos-teteu.herokuapp.com/students/"
 };
 
 /*
